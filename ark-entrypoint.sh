5f207e5f1b91ace37db2304379f7d358a0b7575b
dek <dek@iono.me>
2018-12-23 15:11:58 +0100

    first automod working version

--- ark-entrypoint.sh
+++ ark-entrypoint.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-exec $ARK_DIR/ShooterGame/Binaries/Linux/ShooterGameServer $MAP?listen?SessionName=$SESSION_NAME?ServerPassword=$JOIN_PASSWORD?ServerAdminPassword=$ADMIN_PASSWORD?Port=$GAME_PORT?QueryPort=$QUERY_PORT?MaxPlayers=$MAX_PLAYERS -server -log
+exec $ARK_DIR/ShooterGame/Binaries/Linux/ShooterGameServer $MAP?listen?SessionName=$SESSION_NAME?ServerPassword=$JOIN_PASSWORD?ServerAdminPassword=$ADMIN_PASSWORD?Port=$GAME_PORT?QueryPort=$QUERY_PORT?MaxPlayers=$MAX_PLAYERS -server -log -automanagedmods
